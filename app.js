64d9f6b7947bb4df575491672dd0f6c3e66bc62e
adamleveque91 <adam.leveque0@gmail.com>
2025-10-03 13:44:03 -0500

    added style and the query selctors to grab main

---
+++ app.js
@@ -0,0 +1,8 @@
+const pad = document.querySelector("main");
+
+let getPadSize = prompt(parseInt,"what size do you want your pad?");
+console.log(getPadSize);
+
+function CreatePad(padsize){
+    
+}
